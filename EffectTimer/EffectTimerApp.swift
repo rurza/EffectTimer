c192703fec69b578b0d93c3692c9a6bc609b7de4
Adam Różyński <adam@micropixels.pl>
2021-07-07 20:56:55 +0200

    Initial Commit

---
+++ EffectTimer/EffectTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectTimerApp.swift
+//  EffectTimer
+//
+//  Created by Adam Różyński on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct EffectTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
